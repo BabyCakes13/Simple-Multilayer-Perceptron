4f35f5af0618fc018f0bd5c1307cac28d1bddd6d
Maria-Minerva Vonica <maria.vonica98@gmail.com>
2020-12-26 12:31:37 +0200

    Created the utils file which holds all the activation functions and weights generators.
    Such that each test can get their generators from there instead of having duplicates of each function in each test.

--- tests/main/test_layer.py
+++ tests/main/test_layer.py
@@ -1,4 +1,6 @@
 from main import layer as l
+from . import utils
+
 import unittest
 
 
@@ -9,30 +11,13 @@ class TestLayer(unittest.TestCase):
         neuron_count = [10, 10]
 
         for wc, nc in zip(weights_count, neuron_count):
-            inputs = TestLayer.generate_one_array(wc)
-            weights = TestLayer.generate_one_matrix(wc, nc)
-            activation_functions = [TestLayer.activation_function] * nc
+            inputs = utils.generate_one_array(wc)
+            weights = utils.generate_one_matrix(wc, nc)
+            activation_functions = [utils.activation_function] * nc
 
             layer = l.Layer(weights, activation_functions)
 
             layer_output = layer.compute_output(inputs)
             expected_output = [wc] * nc
 
-            # print(layer_output)
-
             self.assertEqual(expected_output, layer_output)
-
-    def generate_one_array(n):
-        array = [1] * n
-        return array
-
-    def generate_one_matrix(weights_count, neuron_count):
-        matrix = [TestLayer.generate_one_array(weights_count)] * neuron_count
-        return matrix
-
-    def activation_function(x):
-        return max(0, x)
-
-    def generate_layer_activation_functions(neuron_count):
-        activation_functions = [TestLayer.activation_function] * neuron_count
-        return activation_functions
--- tests/main/test_neuron.py
+++ tests/main/test_neuron.py
@@ -1,4 +1,5 @@
 from main import neuron as n
+from . import utils
 
 import unittest
 import random
@@ -6,43 +7,26 @@ import random
 class TestNeuron(unittest.TestCase):
 
     def test_weights_propagation(self):
-        weights = TestNeuron.generate_one_array(100)
+        weights = utils.generate_one_array(100)
         neuron = n.Neuron(weights, None)
         neuron_weights = neuron.get_weights()
 
         self.assertEqual(weights, neuron_weights)
 
     def test_compute_zero_output(self):
-        input = TestNeuron.generate_one_and_minus_one_array(100)
-        weights = TestNeuron.generate_one_array(100)
+        input = utils.generate_one_and_minus_one_array(100)
+        weights = utils.generate_one_array(100)
 
-        neuron = n.Neuron(weights, TestNeuron.activation_function)
+        neuron = n.Neuron(weights, utils.activation_function)
         output = neuron.compute(input)
-
-        # print(weights)
-        # print(input)
 
         self.assertEqual(0, output)
 
     def test_compute_size_output(self):
-        input = TestNeuron.generate_one_array(100)
-        weights = TestNeuron.generate_one_array(100)
+        input = utils.generate_one_array(100)
+        weights = utils.generate_one_array(100)
 
-        neuron = n.Neuron(weights, TestNeuron.activation_function)
+        neuron = n.Neuron(weights, utils.activation_function)
         output = neuron.compute(input)
 
-        # print(weights)
-        # print(input)
-
         self.assertEqual(100, output)
-
-    def activation_function(x):
-        return max(0, x)
-
-    def generate_one_and_minus_one_array(size):
-        array = [1, -1] * (size // 2)
-        return array
-
-    def generate_one_array(size):
-        array = [1] * size
-        return array
