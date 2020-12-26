1efe13ebafd89456ec0ecc401575fdbd88a6d99f
Maria-Minerva Vonica <maria.vonica98@gmail.com>
2020-12-26 12:30:26 +0200

    Added bias to the neuron.
    The bias is chosen as the first weight in the neuron's weights (aka w0).
    A fake input (1) is chosen for this weight.

--- main/neuron.py
+++ main/neuron.py
@@ -1,13 +1,21 @@
 class Neuron:
     def __init__(self, weights, activation_function):
-        self.__weights = weights
+        try:
+            self.__bias = weights[0]
+            self.__weights = weights[1:]
+        except ArrayIndexOutOfBounds as e:
+            print("Weights does not have enough elements to split it into bias and weights.")
+            return
+
         self.__activation_function = activation_function
 
     def compute(self, input):
+        # insert the input for the bias
         sum = 0
-
         for i, w in zip(input, self.__weights):
             sum += i * w
+
+        sum += self.__bias
 
         output = self.__activation_function(sum)
         return output
