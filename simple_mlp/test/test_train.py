12b333c67d3617d4c6e5ad5de7b500fa1dee845c
Maria-Minerva Vonica <maria.vonica98@gmail.com>
2020-12-28 20:35:00 +0200

    Added more tests to train.

--- simple_mlp/test/test_train.py
+++ simple_mlp/test/test_train.py
@@ -1,4 +1,6 @@
 from simple_mlp import train as t
+from simple_mlp import network as n
+import math
 
 import unittest
 import random
@@ -30,6 +32,46 @@ class TestTrain(unittest.TestCase):
             lambda x: 1,
             lambda x: 0 if x < 0 else ( 1 if x > 0 else raise_(Exception("nooooooooo"))) ]
 
-        network = train.setup_network(input_count, network_layout, activation_functions, activation_functions_derivatives)
+        network = train.setup_network(input_count, network_layout,
+                                      activation_functions,
+                                      activation_functions_derivatives)
 
         network.display()
+
+    def test_train(self):
+        dataset = {
+        (2.7810836,2.550537003):   [0],
+        (1.465489372,2.362125076): [0],
+        (3.396561688,4.400293529): [0],
+        (1.38807019	,1.850220317): [0],
+        (3.06407232	,3.005305973): [0],
+        (7.627531214,2.759262235): [1],
+        (5.332441248,2.088626775): [1],
+        (6.922596716,1.77106367):  [1],
+        (8.675418651,-0.242068655):[1],
+        (7.673756466,3.508563011): [1]
+        }
+
+        network_layout = [2, 1]
+        epocs = 20
+        input_count = 2
+        learning_rate = 0.5
+        layer_count = len(network_layout)
+
+        def sigmoid(x):
+          return 1 / (1 + math.exp(-x))
+
+        def sigmoid_derivative(sigmoud_output):
+            return sigmoud_output * (1 - sigmoud_output)
+        activation_functions = [sigmoid] * layer_count
+        activation_functions_derivatives = [sigmoid_derivative] * layer_count
+
+        train = t.Train()
+
+        network = train.setup_network(input_count, network_layout,
+                                      activation_functions,
+                                      activation_functions_derivatives)
+
+        train.set_dataset(dataset)
+        train.display_network()
+        train.chu_chu(learning_rate, 0.1, epocs)
